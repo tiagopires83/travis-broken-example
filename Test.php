59a03a8cbc3413a457a0023f3d69ec5a23c4e62c
Tiago Pires <tiago.pires83@globomail.com>
2016-01-14 09:06:25 -0200

    Update Test.php

--- Test.php
+++ Test.php
@@ -3,12 +3,17 @@
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
-		$this->assertEquals(1+1,3);
+		$this->assertEquals(1+1,2);
   	}
   	
 	public function testOneTimesOne() {
 		$this->assertEquals(1*1,1);
   	}
+  	
+  	public function testOnePlusTwo() {
+		$this->assertEquals(1+2,3);
+  	}
+
 }
 
 ?>
